411bd610aa3b2671fb0f1d8a567ef4ed6c8cb035
AliyevaSalala <aselale58@gmail.com>
2024-02-02 17:03:18 +0300

    I added owl carousel

--- Deluxe-Hotel/client/assets/js/index.js
+++ Deluxe-Hotel/client/assets/js/index.js
@@ -1,27 +1,48 @@
-$(function () {
-  // Owl Carousel
-  var owl = $(".owl-carousel");
-  owl.owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    // autoplay: true,
-    dots: false,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-        loop: true,
-        margin: 20,
-      },
+// OWL-CAROUSEL-ONE
+
+// Owl Carousel
+
+$(".owl-theme").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  // autoplay: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
     },
-  });
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+      loop: true,
+      margin: 20,
+    },
+  },
+});
+
+// OWL-CAROUSEL-TWO
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
 });
