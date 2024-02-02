f4344e135835f77ad8debf0f20cbad71da20c4bd
AliyevaSalala <aselale58@gmail.com>
2024-02-02 03:43:39 +0300

    I added Header Responsibility, Customer Section Carousel

--- Deluxe-Hotel/client/assets/js/common.js
+++ Deluxe-Hotel/client/assets/js/common.js
@@ -1,5 +1,10 @@
 const menuIcon = document.querySelector("#menu-icon");
 const nav = document.querySelector("#nav");
+const menuCircle = document.querySelector(".menu-circle");
+const headerSearch = document.querySelector("#navbar-search");
+const closeIcon = document.querySelector(".close-icon");
+const navbarSearch = document.querySelector(".navbar-search");
+const header = document.querySelector("header");
 
 menuIcon.addEventListener("click", function () {
   nav.classList.toggle("show");
@@ -7,4 +12,27 @@ menuIcon.addEventListener("click", function () {
   this.classList.contains("fa-bars")
     ? (this.className = "fa-solid fa-xmark")
     : (this.className = "fa-solid fa-bars");
+});
+
+menuCircle.addEventListener("click", function () {
+  document.body.classList.toggle("show-menu");
+});
+closeIcon.addEventListener("click", function () {
+  document.body.classList.toggle("show-menu");
+});
+
+// headerSearch.addEventListener("click", function () {
+//   document.body.classList.toggle("show-search");
+// });
+
+// navbarSearch.addEventListener("click", function () {
+//   document.body.classList.toggle("show-search");
+// });
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    header.classList.add("header-scroll");
+  } else {
+    header.classList.remove("header-scroll");
+  }
 });
