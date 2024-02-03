63b4ad697b1976de6bc32318b33f9e4a53035bfa
AliyevaSalala <aselale58@gmail.com>
2024-02-03 13:54:13 +0300

    I added Banner-Section Slide

--- Deluxe-Hotel/client/assets/js/common.js
+++ Deluxe-Hotel/client/assets/js/common.js
@@ -5,6 +5,8 @@ const headerSearch = document.querySelector("#navbar-search");
 const closeIcon = document.querySelector(".close-icon");
 const navbarSearch = document.querySelector(".navbar-search");
 const header = document.querySelector("header");
+// const cursorDot = document.querySelector("[data-cursor-dot]");
+// const cursorOutline = document.querySelector("[data-cursor-outline]");
 
 menuIcon.addEventListener("click", function () {
   nav.classList.toggle("show");
@@ -36,3 +38,22 @@ window.addEventListener("scroll", function () {
     header.classList.remove("header-scroll");
   }
 });
+
+// window.addEventListener("mousemove", function (e) {
+//   const posX = e.clientX;
+//   const posY = e.clientY;
+
+//   cursorDot.style.left = `${posX}px`;
+//   cursorDot.style.top = `${posY}px`;
+
+//   //   cursorOutline.style.left = `${posX}px`;
+//   //   cursorOutline.style.top = `${posY}px`;
+
+//   cursorOutline.animate(
+//     {
+//       left: `${posX}px`,
+//       top: `${posY}px`,
+//     },
+//     { duration: 700, fill: "forwards" }
+//   );
+// });
--- Deluxe-Hotel/client/assets/js/index.js
+++ Deluxe-Hotel/client/assets/js/index.js
@@ -46,3 +46,17 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const slider = document.querySelector(".slider");
+
+next.addEventListener("click", function () {
+  let slides = document.querySelectorAll(".slides");
+  slider.appendChild(slides[0]);
+});
+
+prev.addEventListener("click", function () {
+  let slides = document.querySelectorAll(".slides");
+  slider.prepend(slides[slides.length - 1]);
+});
