6832515caf608c35cb899a10787dfba92b7a037c
AliyevaSalala <aselale58@gmail.com>
2024-02-03 15:44:22 +0300

    blog-section created

--- Deluxe-Hotel/client/assets/js/common.js
+++ Deluxe-Hotel/client/assets/js/common.js
@@ -5,8 +5,8 @@ const headerSearch = document.querySelector("#navbar-search");
 const closeIcon = document.querySelector(".close-icon");
 const navbarSearch = document.querySelector(".navbar-search");
 const header = document.querySelector("header");
-// const cursorDot = document.querySelector("[data-cursor-dot]");
-// const cursorOutline = document.querySelector("[data-cursor-outline]");
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
 
 menuIcon.addEventListener("click", function () {
   nav.classList.toggle("show");
@@ -39,21 +39,21 @@ window.addEventListener("scroll", function () {
   }
 });
 
-// window.addEventListener("mousemove", function (e) {
-//   const posX = e.clientX;
-//   const posY = e.clientY;
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY; 
 
-//   cursorDot.style.left = `${posX}px`;
-//   cursorDot.style.top = `${posY}px`;
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
 
-//   //   cursorOutline.style.left = `${posX}px`;
-//   //   cursorOutline.style.top = `${posY}px`;
+  //   cursorOutline.style.left = `${posX}px`;
+  //   cursorOutline.style.top = `${posY}px`;
 
-//   cursorOutline.animate(
-//     {
-//       left: `${posX}px`,
-//       top: `${posY}px`,
-//     },
-//     { duration: 700, fill: "forwards" }
-//   );
-// });
+  cursorOutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 700, fill: "forwards" }
+  );
+});
