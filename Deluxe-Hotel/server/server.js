86bd363c8d32da28c4b62dbcd5f032e86cbb18ad
AliyevaSalala <aselale58@gmail.com>
2024-02-08 15:20:39 +0300

    Changed Header Section

--- Deluxe-Hotel/server/server.js
+++ Deluxe-Hotel/server/server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const productRouter = require("./routes/productRoutes");
+// const BASE_URL = "mongodb+srv://gd7l74l7n:<password>@cluster0.c5glouy.mongodb.net/";
+
+const productRouter = require("./routes/productRoutes.js");
 
 require("dotenv").config();
 const app = express();
@@ -14,6 +16,8 @@ const DB = process.env.DB_URL;
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
 // });
+
+
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
