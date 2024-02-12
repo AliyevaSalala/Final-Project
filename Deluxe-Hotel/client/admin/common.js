1aead710a907daaa3f5e17cced7e7d93f781efec
AliyevaSalala <aselale58@gmail.com>
2024-02-13 01:36:18 +0300

    arranged to logout in admin-panel.

--- Deluxe-Hotel/client/admin/common.js
+++ Deluxe-Hotel/client/admin/common.js
@@ -3,6 +3,14 @@ const darkMode = document.querySelector(".dark-mode");
 const moonMode = document.querySelector(".dark-mode-moon");
 const body = document.querySelector("body");
 
+const logOut = document.querySelector(".logout");
+
+logOut.addEventListener("click", function () {
+  localStorage.removeItem("isAdmin");
+  window.location.href = "../login.html";
+  // console.log("shalalal");
+});
+
 if (!localStorage.getItem("isAdmin")) {
   window.location.href = "../login.html";
 }
