be8b3d00990664da0b8d205b1934e6a1b7354f07
AliyevaSalala <aselale58@gmail.com>
2024-02-14 18:43:16 +0300

    arranged the counter in admin-dashboard.html

--- Deluxe-Hotel/client/admin/admin.js
+++ Deluxe-Hotel/client/admin/admin.js
@@ -1,8 +1,40 @@
-const userTbdody = document.querySelector(".user-tbody");
-const userSearch = document.querySelector("#user-search");
+const commentCounter = document.querySelector(".comment-counter");
+const reservCounter = document.querySelector(".reserv-counter");
+const menuCounter = document.querySelector(".menu-counter");
+const productCounter = document.querySelector(".product-counter");
+const userCounter = document.querySelector(".user-counter");
 
-// async function getUsersData(endpoint) {
-//   const res = await axios(`${BASE_url}/${endpoint}`);
-//   console.log(res.data);
-// }
-// getUsersData("users");
+
+
+
+
+
+async function updateCounters() {
+  try {
+    const res = await axios(`${BASE_url}/reviews`);
+    const comments = res.data;
+    commentCounter.textContent = comments.length;
+
+    const reservations = await axios(`${BASE_url}/reservations`);
+    const reservationsData = reservations.data;
+    reservCounter.textContent = reservationsData.length;
+
+    const product = await axios(`${BASE_url}/products`);
+    const productsData = product.data;
+    // console.log(product.data);
+    productCounter.textContent = productsData.length;
+
+    const menu = await axios(`${BASE_url}/menu`);
+    const menuData = menu.data;
+    menuCounter.textContent = menuData.length;
+
+    const user = await axios(`${BASE_url}/menu`);
+    // console.log(user.data);
+    const userData = user.data;
+    userCounter.textContent = userData.length;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+updateCounters();
